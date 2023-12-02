9d895579eab6ba12c16226e6122b97d040bd2276
teste <ertist@home>
2023-12-02 17:40:11 +0300

    physics doesnot work

--- server/Vector2.py
+++ server/Vector2.py
@@ -1,13 +1,60 @@
 import math
 
+
+
 class Vector2:
 	def __init__(self, x, y):
 		self.x = x
 		self.y = y
 
 
-	def angle(self, vec):
-		return math.atan2(vec.y - self.y, vec.x - self.x)
+	def distance(self, vec):
+		return ((self.x - vec.x) ** 2 + (self.y - vec.y) ** 2) ** 0.5
+	
+
+	def distance_to_line(self, line_start, line_end):
+		# https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
+		# d = |(x2-x1)(y1-y0)-(x1-x0)(y2-y1)| / sqrt((x2-x1)^2 + (y2-y1)^2)
+		d = abs((line_end.x - line_start.x) * (line_start.y - self.y) - (line_start.x - self.x) * (line_end.y - line_start.y)) / ((line_end.x - line_start.x) ** 2 + (line_end.y - line_start.y) ** 2) ** 0.5
+		return d
+
+	def angle(self):
+		return math.atan2(self.y, self.x)
+
+	def angle_between(self, vec):
+		dot = self.dot(vec)
+		mag = self.mag() * vec.mag()
+		costheta = dot / mag
+		return math.acos(costheta)
+
+	def rotate(self, angle):
+		angle = math.radians(angle)
+		x = self.x * math.cos(angle) - self.y * math.sin(angle)
+		y = self.x * math.sin(angle) + self.y * math.cos(angle)
+		return Vector2(x, y)
+
+	def ray_at(self, ray_start, ray_dir, t):
+		return ray_start + ray_dir * t
+
+	def is_ray_on(self, ray_origin, ray_dir, point):
+		# ray_origin + ray_dir * t = point
+		# t = (point - ray_origin) / ray_dir
+		t = (point - ray_origin) / ray_dir
+
+		return (t>=0)
+
+	def is_point_on_line(self, line_start, line_end, point):
+		# line_start + t * (line_end - line_start) = point
+		# t = (point - line_start) / (line_end - line_start)
+		t = (point - line_start) / (line_end - line_start)
+		return (t>=0 and t<=1)
+
+	def dot(self, vec):
+		return self.x * vec.x + self.y * vec.y
+	
+	def cross(self, vec):
+		result = self.x * vec.y - self.y * vec.x
+		return result
 
 	def add(self, vec):
 		self.x += vec.x
@@ -34,6 +81,7 @@ class Vector2:
 		mag = self.mag()
 		self.x /= mag
 		self.y /= mag
+		return self
 
 	def __str__(self):
 		return "({},{})".format(self.x, self.y)
--- server/pythongame.py
+++ server/pythongame.py
@@ -3,6 +3,44 @@ import math
 from Vector2 import Vector2
 		
 	
+class Edge:
+	def __init__(self, a, b):
+		self.a = a
+		self.b = b
+		self.normal = self.get_normal()
+
+	def get_normal(self):
+		normal = Vector2(self.b.y - self.a.y, self.b.x - self.a.x)
+		normal.normalize()
+		normal = normal * -1
+		return normal
+
+	def get_distance(self, point):
+		# line_start + t * (line_end - line_start) = point
+		# t = (point - line_start) / (line_end - line_start)
+		t = (point - self.a) / (self.b - self.a)
+		return t
+
+	def is_point_on(self, point):
+		# line_start + t * (line_end - line_start) = point
+		# t = (point - line_start) / (line_end - line_start)
+		t = (point - self.a) / (self.b - self.a)
+		return (t>=0 and t<=1)
+
+	def is_ray_on(self, ray_origin, ray_dir, point):
+		# ray_origin + ray_dir * t = point
+		# t = (point - ray_origin) / ray_dir
+		t = (point - ray_origin) / ray_dir
+
+		return (t>=0)
+
+
+	def set_normal(self, normal):
+		self.normal = normal
+
+
+
+
 
 class Rect:
 	def __init__(self, x, y, w, h):
@@ -10,32 +48,76 @@ class Rect:
 		self.w = w
 		self.h = h
 
+		self.top_left = Vector2(x, y)
+		self.top_right = Vector2(x + w, y)
+		self.bottom_left = Vector2(x, y + h)
+		self.bottom_right = Vector2(x + w, y + h)
+
+		self.edges = [
+			Edge(self.top_left, self.top_right),
+			Edge(self.top_right, self.bottom_right),
+			Edge(self.bottom_right, self.bottom_left),
+			Edge(self.bottom_left, self.top_left)
+		]
+
 	def collide(self, obj):
 		if self.pos.x < obj.pos.x + obj.w and self.pos.x + self.w > obj.pos.x and self.pos.y < obj.pos.y + obj.h and self.pos.y + self.h > obj.pos.y:
 			return True
 		return False
 	
+	def edge_normal(self, a, b):
+		normal = Vector2(b.y - a.y, b.x - a.x)
+		normal.normalize()
+		normal = normal * -1
+		return normal
+	
+	def get_intersection_edge(self, obj : "Rect"):
+
+		min_dist = self.edges[0]
+		old = -1
+		for edge in self.edges:
+
+			d = obj.pos.distance_to_line(edge.a, edge.b)
+			if old == -1 or d < old:
+				min_dist = edge
+				old = d
+
+		return min_dist
+	
+
+	def new_pos(self, newpos):
+		self.pos = newpos
+		self.top_left = Vector2(self.pos.x, self.pos.y)
+		self.top_right = Vector2(self.pos.x + self.w, self.pos.y)
+		self.bottom_left = Vector2(self.pos.x, self.pos.y + self.h)
+		self.bottom_right = Vector2(self.pos.x + self.w, self.pos.y + self.h)
+		self.edges = [
+			Edge(self.top_left, self.top_right),
+			Edge(self.top_right, self.bottom_right),
+			Edge(self.bottom_right, self.bottom_left),
+			Edge(self.bottom_left, self.top_left)
+		]
+
+	def change_normal_direction_to_outside(self):
+		for edge in self.edges:
+			if edge.normal.dot(edge.a) < 0:
+				edge.set_normal(edge.normal * -1)
+	
+
 class GameObj(Rect):
 	def __init__(self, x, y, w, h, name="gameobj", static=False):
 		super().__init__(x, y, w, h)
+		self.last_pos = Vector2(x, y)
 		self.velocity = Vector2(0, 0)
-		self.momentum = Vector2(0, 0)
 		self.acceleration = Vector2(0, 0)
 		self.mass = 1
+		self.friction = 0.01
 		self.static = static
 
 		self.name = name
 		self.stage = "init"
-	
-	def apply_force(self, force : Vector2):
-		self.acceleration += force / self.mass
 
-	def update_velocity(self, dt=1):
-		self.velocity += self.acceleration * dt
 
-	def update_position(self, dt = 1):
-		if not self.static:
-			self.pos += self.velocity * dt
 
 
 	def update_collision(self, obj_list):
@@ -44,36 +126,60 @@ class GameObj(Rect):
 				continue
 			if self.collide(obj):
 				self.on_collide(obj)
+				if self.static:
+					continue
+				
+				edge = obj.get_intersection_edge(self)
+				normal = edge.normal
 
-				collide_angle = self.pos.angle(obj.pos)
-				collide_angle = math.degrees(collide_angle)
+				angle = self.velocity.angle_between(normal)
+				cos = math.cos(angle)
+				sin = math.sin(angle)
 
-				#f = Vector2((self.velocity.x * math.cos(collide_angle)), (self.velocity.y * math.sin(collide_angle)))
-				f = (obj.velocity * obj.mass ) - (Vector2(self.velocity * self.mass).x * math.cos(collide_angle), Vector2(self.velocity * self.mass).y * math.sin(collide_angle))
-				self.velocity += f
-				obj.velocity += f
-				#obj.apply_force(f)
+				fx = self.velocity.x * cos
+				fy = self.velocity.y * sin
+				f = Vector2(fx, fy)
+				print(f  * -1)
+				self.set_velocity(f + self.velocity, 1)
 
-				obj_list.remove(obj)
+	def get_collision_force(self, obj):
+		# f = (m1 * v1) + (m2 * v2)
+		f = (self.velocity * self.mass) + (obj.velocity * obj.mass)
+		return f
+
+	def apply_force(self, force, dt=1):
+		self.acceleration += force / self.mass
 
 
 
-				#obj.apply_force(obj.velocity * self.mass * self.velocity)
+	def update_position(self, dt=1):
+		newpos =  ((self.velocity * dt) + (self.acceleration * 0.5) * (dt ** 2)) + self.pos
+		self.new_pos(newpos)
+
 
 	def tick(self, dt=1, obj_list = []):
 		if self.stage == "init":
 			self.on_init()
 			self.stage = "update"
-		self.update_velocity(dt)
+
 		self.update_collision(obj_list)
+		self.update(dt)
 		self.update_position(dt)
+
+	def set_velocity(self, velocity, dt=1):
+		self.velocity = velocity
+		self.acceleration = Vector2(0, 0)
+
+	def get_velocity(self):
+		return self.velocity
+	
 
 	def update(self, dt=1):
 		pass
 
 	def on_init(self):
-		self.velocity = Vector2(1, 1)
-
+		self.set_velocity(Vector2(0.1, 0.1), 1)
+		pass
 	def on_collide(self, obj):
 		pass
 
@@ -93,9 +199,7 @@ font = pygame.font.SysFont("Arial", 30)
 
 obj_list = []
 
-rect = GameObj(100, 100, 10, 10, "ball")
-rect2 = GameObj(100, 150, 10, 10, "ball2")
-rect3 = GameObj(50, 120, 10, 10, "ball3")
+rect = GameObj(200, 200, 10, 10, "ball")
 
 border_top = GameObj(0, 0, 400, 10, "border_top", True)
 border_bottom = GameObj(0, 390, 400, 10, "border_bottom", True)
@@ -110,12 +214,15 @@ border_right.mass = 100000
 
 
 obj_list.append(rect)
-obj_list.append(rect2)
-obj_list.append(rect3)
 obj_list.append(border_top)
 obj_list.append(border_bottom)
 obj_list.append(border_left)
 obj_list.append(border_right)
+
+
+def draw_line_by_angle(screen, color, start_pos, length, angle):
+	end_pos = (start_pos[0] + length * math.cos(angle), start_pos[1] + length * math.sin(angle))
+	pygame.draw.line(screen, color, start_pos, end_pos)
 
 while True:
 	for event in pygame.event.get():
@@ -123,16 +230,12 @@ while True:
 			pygame.quit()
 			quit()
 	screen.fill((0, 0, 0))
-	rect.tick(1, obj_list)
-	rect2.tick(1, obj_list)
-	rect3.tick(1, obj_list)
+	delta = clock.get_time()
+	rect.tick(delta, obj_list)
 	pygame.draw.rect(screen, (255, 255, 255), (rect.pos.x, rect.pos.y, rect.w, rect.h))
-	pygame.draw.rect(screen, (255, 255, 255), (rect2.pos.x, rect2.pos.y, rect2.w, rect2.h))
-	pygame.draw.rect(screen, (255, 255, 255), (rect3.pos.x, rect3.pos.y, rect3.w, rect3.h))
 	pygame.draw.rect(screen, (255, 255, 255), (border_top.pos.x, border_top.pos.y, border_top.w, border_top.h))
 	pygame.draw.rect(screen, (255, 255, 255), (border_bottom.pos.x, border_bottom.pos.y, border_bottom.w, border_bottom.h))
 	pygame.draw.rect(screen, (255, 255, 255), (border_left.pos.x, border_left.pos.y, border_left.w, border_left.h))
 	pygame.draw.rect(screen, (255, 255, 255), (border_right.pos.x, border_right.pos.y, border_right.w, border_right.h))
-
 	pygame.display.update()
 	clock.tick(60)
